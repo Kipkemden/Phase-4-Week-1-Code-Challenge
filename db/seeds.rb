1cbf48523f2879a06f1ded89e3c691cc291a8427
Cheruiyot Kipkemoi <d.cheru112@gmail.com>
2023-07-07 16:14:13 +0300

    seeding info

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Create Restaurants
+restaurant1 = Restaurant.create(name: "Sottocasa NYC", address: "298 Atlantic Ave, Brooklyn, NY 11201")
+restaurant2 = Restaurant.create(name: "PizzArte", address: "69 W 55th St, New York, NY 10019")
+
+# Create Pizzas
+pizza1 = Pizza.create(name: "Cheese", ingredients: "Dough, Tomato Sauce, Cheese")
+pizza2 = Pizza.create(name: "Pepperoni", ingredients: "Dough, Tomato Sauce, Cheese, Pepperoni")
+
+# Create RestaurantPizzas
+restaurant_pizza1= RestaurantPizza.create(price: 10, restaurant: restaurant1, pizza: pizza1)
+restaurant_pizza2= RestaurantPizza.create(price: 12, restaurant: restaurant1, pizza: pizza2)
+restaurant_pizza3= RestaurantPizza.create(price: 8, restaurant: restaurant2, pizza: pizza1)
+
+puts "Seeding completed successfully."
